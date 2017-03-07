59d4259acabb24a6c703eb8a9a131e960f2acabe
Bao Nguyen <nqbaoit@gmail.com>
2017-03-07 15:00:34 +0700

    - update

--- Dao/KhachHangDao.cs
+++ Dao/KhachHangDao.cs
@@ -48,8 +48,8 @@ namespace OrderApp.Dao
                 + ", CREATE_BY"
                 + ", CREATE_TIME"
                 + ")" 
-                + "VALUES ("
-                + "@idKhachHang"
+                + " VALUES ("
+                + " @idKhachHang"
                 + ", @tenKhachHang"
                 + ", @diaChi"
                 + ", @email"
@@ -80,6 +80,68 @@ namespace OrderApp.Dao
             cmd.Parameters.AddWithValue("@trangthaixuatkho", dto.trangThaiXuatKho);
             cmd.Parameters.AddWithValue("@createBy", dto.createBy);
             cmd.Parameters.AddWithValue("@createTime", dto.createTime);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void insertList(List<KhachHangDto> listKH)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = Connection.getConnection();
+            String sql = "INSERT INTO KHACH_HANG "
+                + "("
+                + " ID_KHACH_HANG"
+                + ", TEN_KHACH_HANG"
+                + ", DIA_CHI"
+                + ", EMAIL"
+                + ", ACC_FTP"
+                + ", GIAM_GIA"
+                + ", TEN_SALES"
+                + ", HOA_HONG_SALES"
+                + ", GHI_CHU"
+                + ", NGAY_BAT_DAU"
+                + ", VAN_CHUYEN"
+                + ", TRANG_THAI"
+                + ", CREATE_BY"
+                + ", CREATE_TIME"
+                + " )"
+                + " VALUES ";
+            for (int i=0; i < listKH.Count; i++)
+            {
+                sql += "("
+                + " @idKhachHang" + i
+                + ", @tenKhachHang" + i
+                + ", @diaChi" + i
+                + ", @email" + i
+                + ", @accFtp" + i
+                + ", @giamGia" + i
+                + ", @sales" + i
+                + ", @salesPercent" + i
+                + ", @notes" + i
+                + ", @startDate" + i
+                + ", @vanChuyen" + i
+                + ", @trangthaixuatkho" + i
+                + ", @createBy" + i
+                + ", @createTime" + i
+                + " )";
+
+                KhachHangDto dto = listKH[i];
+
+                cmd.Parameters.AddWithValue("@idKhachHang" + i, dto.idKhachHang);
+                cmd.Parameters.AddWithValue("@tenKhachHang" + i, dto.tenKhachHang);
+                cmd.Parameters.AddWithValue("@diaChi" + i, dto.diaChi);
+                cmd.Parameters.AddWithValue("@email" + i, dto.email);
+                cmd.Parameters.AddWithValue("@accFtp" + i, dto.accFtp);
+                cmd.Parameters.AddWithValue("@giamGia" + i, dto.giamGia);
+                cmd.Parameters.AddWithValue("@sales" + i, dto.sales);
+                cmd.Parameters.AddWithValue("@salesPercent" + i, dto.salesPercent);
+                cmd.Parameters.AddWithValue("@notes" + i, dto.notes);
+                cmd.Parameters.AddWithValue("@startDate" + i, dto.startDate);
+                cmd.Parameters.AddWithValue("@vanChuyen" + i, dto.vanChuyen);
+                cmd.Parameters.AddWithValue("@trangthaixuatkho" + i, dto.trangThaiXuatKho);
+                cmd.Parameters.AddWithValue("@createBy" + i, dto.createBy);
+                cmd.Parameters.AddWithValue("@createTime" + i, dto.createTime);
+            }
             cmd.ExecuteNonQuery();
         }
 
