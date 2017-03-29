9e65c52dce2bb8f48f88ba1993ea3e1aa9d8358b
fuko9x <lvbinh.working@gmail.com>
2017-03-29 18:33:03 +0700

    xxx

---
+++ FormView/DemoExcel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OrderApp.FormView
+{
+    public partial class DemoExcel : Form
+    {
+        public DemoExcel()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp = new Excel.Application();
+
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not properly installed!!");
+                return;
+            }
+            
+
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            xlWorkBook = xlApp.Workbooks.Open("D:\\CongNoTemplate.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            
+            for (int i = 1; i < 5; i++)
+            {
+                var r = xlWorkSheet.get_Range(string.Format("{0}:{0}", 12 + i, Type.Missing));
+                var range = xlWorkSheet.get_Range(string.Format("{0}:{0}", 11, Type.Missing));
+                range.Select();
+                range.Copy();
+                r.Insert();
+                xlWorkSheet.Cells[11 + i - 1, 1] = i;
+            }
+           
+
+
+            xlWorkBook.SaveAs("d:\\CongNo.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+            MessageBox.Show("Excel file created , you can find the file d:\\csharp-Excel.xls");
+        }
+    }
+}
